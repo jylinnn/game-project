d9ff7a52fb96a6b01b6d196526636d2236d9778d
jylinnn <jylin911@gmail.com>
2025-11-05 14:02:41 -0800

    levels exist

--- Assets/Playercontrols.cs
+++ Assets/Playercontrols.cs
@@ -17,8 +17,8 @@ public class Playercontrols : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start()
     {
-        score = 0;
-        scoreText.text = "Score: " + score.ToString();
+        score = 1;
+        scoreText.text = "Level: " + score.ToString();
         hitbox = GetComponent<Rigidbody>();
 
         XSpeed = 0f;
@@ -53,6 +53,6 @@ public class Playercontrols : MonoBehaviour
     }
     void SetScoreText()
     {
-        scoreText.text = "Score: " + score.ToString();
+        scoreText.text = "Level: " + score.ToString();
     }
 }
